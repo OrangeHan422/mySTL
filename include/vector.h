406c9c2746239bfc63cb0c88d9900f692a0b6407
Orange Han <71255902133@stu.ecnu.edu.cn>
2024-04-13 13:24:34 +0800

    vector inplement

---
+++ include/vector.h
@@ -0,0 +1,164 @@
+#pragma once
+
+
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <stdexcept>
+
+
+namespace myStd{
+
+template <typename Elem>
+class vector{
+public:
+    //ctor
+    vector()
+    :m_elemnt_ptr(nullptr)
+    ,m_capacity(0)
+    ,m_size(0)
+    {}
+
+    //dtor
+    ~vector(){ _cleanup(); }
+
+    //copy ctor
+    vector(const vector& rhs)
+    :m_capacity(rhs.m_capacity)
+    ,m_size(rhs.m_size)
+    {
+        m_elemnt_ptr = new Elem[m_capacity];
+        std::copy(rhs.m_elemnt_ptr,rhs.m_elemnt_ptr+m_size,m_elemnt_ptr);
+    }
+
+    //assignment
+    vector& operator=(const vector& rhs){
+        if(this != &rhs){
+            delete[] m_elemnt_ptr;
+            m_capacity = rhs.m_capacity;
+            m_size = rhs.m_size;
+            m_elemnt_ptr = new Elem[m_capacity];
+            std::copy(rhs.m_elemnt_ptr,rhs.m_elemnt_ptr + m_size,m_elemnt_ptr);
+        }
+        return *this;
+    }
+
+    //member function
+    void push_back(const Elem& value){
+        if(m_size == m_capacity){
+            _realloac(m_capacity == 0 ? 1 : 2 * m_capacity);
+        }
+        m_elemnt_ptr[m_size++] = value;
+    }
+
+    size_t size(){
+        return m_size;
+    }
+
+    size_t capacity(){
+        return m_capacity;
+    }
+
+    //accessment
+    Elem& operator[](size_t index){
+        return m_elemnt_ptr[index];
+    }
+
+    Elem& at(size_t index){
+        if(index >= size){
+            throw std::out_of_range("index out of range");
+        }
+        return m_elemnt_ptr[index];
+    }
+
+    const Elem& operator[](size_t index) const {
+        return m_elemnt_ptr[index];
+    }
+
+    const Elem& at(size_t index) const {
+        if(index >= size){
+            throw std::out_of_range("index out of range");
+        }
+        return m_elemnt_ptr[index];
+    }
+
+    //insert
+    void insert(size_t index,const Elem& value){
+        if(index > m_size){
+            throw std::out_of_range("index out of range");
+        }
+        if(m_size == m_capacity){
+            _realloac(m_capacity == 0 ? 1 : 2 * m_capacity);
+        }
+        for(size_t i = m_size; i < index; --i){
+            //all elem behind index should be 1 step backward
+            m_elemnt_ptr[i] = m_elemnt_ptr[i-1];
+        }
+        m_elemnt_ptr[index] = value;
+        ++m_size;
+    }
+
+    // delete
+    void pop_back(){
+        if(m_size > 0){
+            --m_size;
+        }
+    }
+
+    void clear(){
+        m_size = 0;
+    }
+
+    Elem* begin(){
+        return m_elemnt_ptr;
+    }
+
+    Elem* end(){
+        return m_elemnt_ptr + m_size;
+    }
+
+    const Elem* cbegin() const {
+        return m_elemnt_ptr;
+    }
+
+    const Elem* cend() const {
+        return m_elemnt_ptr + m_size;
+    }
+
+    void printElements() const
+    {
+        for (size_t i = 0; i < m_size; ++i)
+        {
+            std::cout << m_elemnt_ptr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+private:
+    Elem* m_elemnt_ptr;
+    size_t m_capacity;
+    size_t m_size;
+
+private:
+    void _cleanup(){
+        delete[] m_elemnt_ptr;
+        m_elemnt_ptr = nullptr;
+        m_capacity = 0;
+        m_size = 0;
+    }
+
+    void _realloac(size_t newCapacity){
+        if(newCapacity > m_capacity){
+            Elem* new_element_ptr = new Elem[newCapacity];
+            std::copy(m_elemnt_ptr,m_elemnt_ptr+m_size,new_element_ptr);
+            delete[] m_elemnt_ptr;
+            m_elemnt_ptr = new_element_ptr;
+            m_capacity = newCapacity;
+        }
+    }
+    
+
+};
+
+}//namespace myStd
---
+++ main.cpp
@@ -0,0 +1,94 @@
+#include "vector.h"
+
+using namespace myStd;
+void testVector(){
+   // 创建一个 Vector 对象
+    vector<int> myVector;
+
+    int N;
+    std::cin >> N;
+    // 读走回车
+    getchar();
+
+    std::string line;
+    for (int i = 0; i < N; i++)
+    {
+        // 读取整行
+        std::getline(std::cin, line);
+        std::istringstream iss(line);
+        std::string command;
+        iss >> command;
+
+        if (command == "push")
+        {
+            int value;
+            iss >> value;
+            myVector.push_back(value);
+        }
+        else if (command == "print")
+        {
+            if (myVector.size() == 0) {
+                std::cout << "empty" << std::endl;
+                continue;
+            }
+            myVector.printElements();
+        }
+        else if (command == "size")
+        {
+            std::cout << myVector.size() << std::endl;
+        }
+        else if (command == "get")
+        {
+            int index;
+            iss >> index;
+            std::cout << myVector[index] << std::endl;
+        }
+        else if (command == "insert")
+        {
+            int index, value;
+            iss >> index >> value;
+            myVector.insert(index, value);
+        }
+        else if (command == "pop")
+        {
+            myVector.pop_back();
+        }
+        else if (command == "iterator")
+        {
+            if (myVector.size() == 0)
+            {
+                std::cout << "empty" << std::endl;
+                continue;
+            }
+            for (auto it = myVector.begin(); it != myVector.end(); ++it)
+            {
+                std::cout << *it << " ";
+            }
+            std::cout << std::endl;
+        }
+        else if (command == "foreach")
+        {
+            if (myVector.size() == 0)
+            {
+                std::cout << "empty" << std::endl;
+                continue;
+            }
+            for (const auto &element : myVector)
+            {
+                std::cout << element << " ";
+            }
+            std::cout << std::endl;
+        }
+        else if (command == "clear")
+        {
+            myVector.clear();
+        }
+    }
+}
+
+
+int main()
+{
+    // testVector();
+    return 0;
+}
